ceb142289e7e27e1991dd4e111afeaf1524ed60e
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-04-14 06:55:03 +0800

    Add
    Add

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label1.Location = new System.Drawing.Point(148, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "□□□□□□□";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label2.Location = new System.Drawing.Point(148, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "□□□□□□□";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label3.Location = new System.Drawing.Point(148, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(372, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "□□□□□□□";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label4.Location = new System.Drawing.Point(148, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(372, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "□□□□□□□";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label5.Location = new System.Drawing.Point(148, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(372, 50);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "□□□□□□□";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 30F);
+            this.label6.Location = new System.Drawing.Point(148, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(372, 50);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "□□□□□□□";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(38, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "輪到:○";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 20F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(217, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(218, 43);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "   選擇 第...行";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(157, 452);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(400, 452);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 38);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(-1, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 37);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "help";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 535);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Connect4 board";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connect
+{
+    public partial class Form1 : Form
+    {
+        public class Global
+        {
+            public static bool isFilled = false;  //false='○'棋  true ='●'棋
+            public static string[,] x = new string[6, 7];  //棋盤
+            public static int[] isConnect = new int[69];  //是否連線
+            public static int[,] Connect_loc = new int[69, 8];  //69條連線的XY座標
+            public static char[] text = new char[] { '○', '●' };
+            public static string[] str_ordinal = new string[] { "一", "二", "三", "四", "五", "六", "七" };
+            public static Random rnd = new Random();
+            public static int button_click_time = 0;  //棋步數:為了避免滿棋盤
+        }
+        public void set_Connect_loc()//建立所有連線的4點座標
+        {
+            for (int i = 0; i < 21; i++)//直  左到右 下到上
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Global.Connect_loc[i, 2 * j] = 5 - (j + i / 7);
+                    Global.Connect_loc[i, 2 * j + 1] = i % 7;
+                }
+            }
+            for (int i = 21; i < 45; i++)//橫  左到右 下到上
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Global.Connect_loc[i, 2 * j] = 5 - ((i - 21) / 4);
+                    Global.Connect_loc[i, 2 * j + 1] = (i - 21) % 4 + j;
+                }
+            }
+            for (int i = 45; i < 57; i++)//斜右上  左到右 下到上
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Global.Connect_loc[i, 2 * j] = 5 - ((i - 45) / 4 + j);
+                    Global.Connect_loc[i, 2 * j + 1] = (i - 45) % 4 + j;
+                }
+            }
+            for (int i = 57; i < 69; i++)//斜左上  左到右 下到上
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Global.Connect_loc[i, 2 * j] = 5 - ((i - 57) / 4 + j);
+                    Global.Connect_loc[i, 2 * j + 1] = (i - 57) % 4 + (3 - j);
+                }
+            }
+        }
+        public void check_Connect()//檢查所有連線
+        {
+            for (int i = 0; i < 69; i++)
+            {
+                if (Global.isConnect[i] == 0)
+                {
+                    string _1 = Global.x[Global.Connect_loc[i, 0], Global.Connect_loc[i, 1]];
+                    string _2 = Global.x[Global.Connect_loc[i, 2], Global.Connect_loc[i, 3]];
+                    string _3 = Global.x[Global.Connect_loc[i, 4], Global.Connect_loc[i, 5]];
+                    string _4 = Global.x[Global.Connect_loc[i, 6], Global.Connect_loc[i, 7]];
+                    if ((_1 + _2 + _3 + _4).Contains("○") && (_1 + _2 + _3 + _4).Contains("●"))//某條連線包含2種棋=此連線不會達成->不需要再檢查
+                    {
+                        Global.isConnect[i] = -1;
+                    }
+                    else if ((_1 + _2 + _3 + _4) == "○○○○" || (_1 + _2 + _3 + _4) == "●●●●")//某條連線某棋贏了
+                    {
+                        Global.isConnect[i] = 1;
+                        End_to_Win();
+                    }
+                }
+            }
+        }
+        public void reset_isConnect()//把所有連線的檢查回到原始狀態
+        {
+            for (int i = 0; i < 69; i++)
+            {
+                Global.isConnect[i] = 0;
+            }
+        }
+        public void End_to_Win()//XX贏了
+        {
+            MessageBox.Show(Global.text[Convert.ToInt32(!Global.isFilled)] + " is Winner");
+            resetBoard();
+        }
+        public void resetBoard()//棋盤復原+一些些東西復原
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Global.x[i, j] = "□";
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                printBoard(i);
+            }
+            reset_isConnect();
+            Global.isFilled = false;
+            Global.button_click_time = 0;
+            label8.Text = "   選擇 第...行";
+            label7.Text = "輪到:○";
+
+        }
+        public void printBoard(int r) //print每一行的Text
+        {
+            string temp_str = Global.x[r, 0] + Global.x[r, 1] + Global.x[r, 2] + Global.x[r, 3] + Global.x[r, 4] + Global.x[r, 5] + Global.x[r, 6];
+            switch (r)
+            {
+                case 0:
+                    label1.Text = temp_str;
+                    break;
+                case 1:
+                    label2.Text = temp_str;
+                    break;
+                case 2:
+                    label3.Text = temp_str;
+                    break;
+                case 3:
+                    label4.Text = temp_str;
+                    break;
+                case 4:
+                    label5.Text = temp_str;
+                    break;
+                case 5:
+                    label6.Text = temp_str;
+                    break;
+            }
+        }
+        public int _ok(int r, int c) //判斷該直行是否可以繼續放
+        {
+            if (Global.x[r, c - 1] == "□")
+                return r;  //輸出高度
+            else if (r == 0) //超出棋格
+                return 100;
+            else
+                return _ok(r - 1, c);  //該格被占 遞迴尋找
+        }
+        public void insert(int c)
+        {
+            int temp_R = _ok(5, c);
+            Global.x[temp_R, c - 1] = Convert.ToString(Global.text[Convert.ToInt32(Global.isFilled)]);  //某格的棋子顏色
+            Global.isFilled = !Global.isFilled;  //換顏色棋子
+            printBoard(temp_R);  //某橫行變動Print某行 省電
+            if (Global.button_click_time >= 8)  //至少8步(各4顆)再檢查連線 省時間 省電
+            {
+                check_Connect();
+            }
+        }
+        public void makeAmove()
+        {
+            int c;  //隨機放棋
+            do
+            {
+                c = Global.rnd.Next(1, 8);
+            } while (Global.x[0, c - 1] != "□");
+            label8.Text = Global.text[Convert.ToInt32(Global.isFilled)] + "選擇 第" + Global.str_ordinal[c - 1] + "行";
+            label7.Text = "輪到:" + Global.text[Convert.ToInt32((!Global.isFilled))];
+            insert(c);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            //label8.Text = 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Global.button_click_time < 42)
+            {
+                if (Global.button_click_time == 0) //初始化
+                {
+                    resetBoard();
+                    set_Connect_loc();
+                }
+                Global.button_click_time += 1;
+                makeAmove();
+                if (Global.button_click_time == 42) //42滿棋
+                {
+                    label8.Text = "         滿了";
+                    label7.Text = "滿了";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //reset
+        {
+            resetBoard();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("輪 流 放 棋，\n棋 子 會 落 至 最 底，\n4 棋 連 線 者 贏", "Help", MessageBoxButtons.OK,MessageBoxIcon.Question);
+        }
+    }
+}
